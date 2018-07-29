ab691c281f94c143039f7d09aa43cb3397ef88d3
chrissetiana <chrissetiana@gmail.com>
2018-07-29 12:17:11 +0100

    Add view types

--- app/src/main/java/com/chrissetiana/minigallery/MainActivity.java
+++ app/src/main/java/com/chrissetiana/minigallery/MainActivity.java
@@ -2,6 +2,9 @@ package com.chrissetiana.minigallery;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView viewCard = findViewById(R.id.gallery_card);
+        viewCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You choose card view", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
